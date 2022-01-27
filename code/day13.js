ae93891faf21f0cc7133ac6272dbfed64f5eb4d7
abhilash <abhilash.kamble376@gmail.com>
2022-01-27 22:13:17 +0530

    day13

---
+++ code/day13.js
@@ -0,0 +1,42 @@
+// let, var, const
+
+// ========= let ===============
+//numebrs
+let myNumber = 10
+console.log("🚀 ~ file: day13.js ~ line 4 ~ myNumber", typeof(myNumber))
+console.log("🚀 ~ file: day13.js ~ line 9 ~ myNumber", myNumber)
+
+//strings
+let myString = "rayn steve"
+console.log("🚀 ~ file: day13.js ~ line 9 ~ myString", typeof(myString))
+console.log("🚀 ~ file: day13.js ~ line 15 ~ myString", myString)
+
+//array
+let cars=['i20','i10','creta']
+console.log("🚀 ~ file: day13.js ~ line 14 ~ cars", cars)
+console.log("🚀 ~ file: day13.js ~ line 14 ~ cars", typeof(cars))
+
+
+// ======== var ============
+var myValue=500   //string, object
+console.log("🚀 ~ file: day13.js ~ line 21 ~ myValue", myValue)
+
+
+// ====== const =========
+const name = "steve" 
+
+
+
+// alert("this is an alert")
+
+// const result = confirm("do you want to submit") 
+
+// if(result){
+//     console.log("hello world")
+// }
+// else{
+//     console.log("bye bye")
+// }
+
+const username= window.prompt("enter your name")
+console.log("🚀 ~ file: day13.js ~ line 42 ~ username", username)
